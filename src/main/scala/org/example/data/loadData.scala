ddf1c3f1c1d850a7cdfdc9152337323dc978bbc0
Sindhu Patchipulusu <sindhu245>
2023-07-24 00:51:29 +0530

    Full code

--- src/main/scala/org/example/data/loadData.scala
+++ src/main/scala/org/example/data/loadData.scala
@@ -7,17 +7,17 @@ class loadData{
 
     def loadDataset_1(): DataFrame = {
       //dataset 1
-//      val dataset_1 = new Dataset_1()
-//      val schema = Seq("id", "score", "price", "attributes", "timeStamp", "expiry", "genre")
-//      writeDataframe(dataset_1.getDataset1(schema) ,"dataset_1")     //Write dataframe
+      val dataset_1 = new Dataset_1()
+      val schema = Seq("id", "score", "price", "attributes", "timeStamp", "expiry", "genre")
+      writeDataframe(dataset_1.getDataset1(schema) ,"dataset_1")     //Write dataframe
       readDataframe("dataset_1") //read dataframe from mysql
     }
 
     def loadDataset_2(): DataFrame= {
         //dataset 2
-//      val dataset_2 = new Dataset_2()
-//      val schema = Seq("genreId", "genreName")
-//      writeDataframe(dataset_2.getDataset2(schema),"dataset_2")   //Write dataframe
+      val dataset_2 = new Dataset_2()
+      val schema = Seq("genreId", "genreName")
+      writeDataframe(dataset_2.getDataset2(schema),"dataset_2")   //Write dataframe
       readDataframe("dataset_2")     //read dataframe from mysql
     }
 
