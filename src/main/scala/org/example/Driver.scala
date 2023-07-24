e948ab97de3a68e4d4b7c72df1ee1a43bcd5409f
Sindhu Patchipulusu <sindhu245>
2023-07-24 11:50:05 +0530

    Creating empty repo

--- src/main/scala/org/example/Driver.scala
+++
@@ -1,9 +0,0 @@
-package org.example
-
-import org.apache.spark.sql.DataFrame
-
-trait Driver {
-      def load():List[DataFrame]
-      def transform(dataFrames: List[DataFrame]):DataFrame
-      def write(finalDataset:DataFrame)
-}
